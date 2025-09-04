46c8727202a7467746e6f0314218deaef4ce3f59
Asep Riezky <98480691+AsepRizz@users.noreply.github.com>
2025-09-05 00:39:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 # ASEPOSINT Installation Script for Termux
 
 echo -e "\033[91m"
@@ -13,11 +13,22 @@ pkg update -y && pkg upgrade -y
 
 # Install dependencies
 echo -e "\033[93m[+] Installing dependencies...\033[0m"
-pkg install -y python git curl nmap whois exiftool
+pkg install -y python git curl nmap python-pip perl make golang
 
 # Install Python packages
 echo -e "\033[93m[+] Installing Python packages...\033[0m"
 pip install requests beautifulsoup4 python-whois
+
+# Install ExifTool from source
+echo -e "\033[93m[+] Installing ExifTool...\033[0m"
+wget https://exiftool.org/Image-ExifTool-12.85.tar.gz
+tar xzf Image-ExifTool-12.85.tar.gz
+cd Image-ExifTool-12.85
+perl Makefile.PL
+make
+make install
+cd ..
+rm -rf Image-ExifTool-12.85 Image-ExifTool-12.85.tar.gz
 
 # Install external tools
 echo -e "\033[93m[+] Installing external tools...\033[0m"
@@ -48,7 +59,8 @@ fi
 if [ ! -f "/data/data/com.termux/files/usr/bin/whatweb" ]; then
     git clone https://github.com/urbanadventurer/WhatWeb.git
     cd WhatWeb
-    make install
+    chmod +x whatweb
+    cp whatweb /data/data/com.termux/files/usr/bin/
     cd ..
 fi
 
